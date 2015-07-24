81cfa2a56100449b22cd3e2b9f5764ab22d95904
Mattburford <Matthew@burfords.org>
2015-07-24 12:41:19 +0100

    Update drone-backend.js

--- drone-backend.js
+++ drone-backend.js
@@ -12,41 +12,52 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "androne-nav",
+        connection: "androne"
+    })
+    .on("ready", fly);
 
-    .on("ready", fly)
-.device("nav", {
-    driver: "androne-nav",
-    connection: "ardrone"
-})
-.on("ready",fly);
 
 var bot;
 // Fly the bot
 function fly(robot) {
     bot = robot;
+
+
     bot.drone.config('general:navdata_demo', 'TRUE');
-    bot.nav.on("navdata", function(data){
+    bot.nav.on("navdata", function (data) {
         //console.log(data);
     });
-    bot.nav.on("altitudeChange", function(data) {
+    bot.nav.on("altitudeChange", function (data) {
         console.log("Altitude:", data);
         // Drone is higher than 1.5 meters up
-        if (altitude > 1.5) {
-            bot.drone.land();
-        }
+       // if (data > 1.5) {
+       //     bot.drone.land();
+        //}
     });
-    bot.nav.on("batteryChange", function(data){
+    bot.nav.on("batteryChange", function (data) {
         console.log("Battery level:", data);
     });
+
+
     bot.drone.disableEmergency();
     bot.drone.ftrim();
     bot.drone.takeoff();
-    after(10* 1000, function () {
+
+    after(2 * 1000, function () {
+        bot.drone.front(0.05)
+    });
+    after(6 * 1000, function () {
+        bot.drone.left(0.05)
+    });
+    after(10 * 1000, function () {
         bot.drone.land();
     });
-after(15*1000, function(){
-    bot.drone.stop();
-});
+
+    after(15 * 1000, function () {
+        bot.drone.stop();
+    });
 //jdk
 }
 
