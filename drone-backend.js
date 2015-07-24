d13657384eff7184388a4f69880a913b1a921b9c
V3493 (Conference ID) <v3493@cam.ac.uk>
2015-07-24 11:06:33 +0100

    drone ollie

--- drone-backend.js
+++ drone-backend.js
@@ -1,5 +1,5 @@
 var Cylon = require('cylon');
-var ws = require('nodejs-websocket');
+//var ws = require('nodejs-websocket');
 var bot;
 
 // Initialise the robot
@@ -12,11 +12,47 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
-    .on("ready", fly);
-    
+
+.device("drone", {
+    driver: "ardrone",
+    connection: "ardrone"
+})
+    .on("ready", fly)
+.device("nav", {
+    driver: "androne-nav",
+    connection: "ardrone"
+});
+
+var bot;
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data){
+        console.log(data);
+    });
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+    bot.nav.on("batteryChange", function(data){
+        concsole.log("Battery level:", data);
+    });
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff(0.5);
+    after(5* 1000, function () {
+        bot.drone.land(0.5);
+    });
+after(15*1000, function(){
+    bot.drone.stop();
+});
 
 }
 
 Cylon.start();
+
+
